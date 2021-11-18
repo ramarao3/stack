88f9a7c33c84e113d5227482e34743dad3b407f7
Rama Rao Adapa (MINDTREE LIMITED) <v-radapa@microsoft.com>
2021-11-18 12:05:45 +0530

    features

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Azure Devops
+      </header>
+    </div>
+  );
+}
+
+export default App;
